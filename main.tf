1300751ea2b68f5cd175ed5c1b6853ef1fc5d08d
abdel-farah96 <abdulfarah1996@gmail.com>
2025-05-03 16:25:17 -0600

    Adding tfstate to s3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+      bucket         = "sandbox-tf-bucket"
+      key            = "terraform.tfstate"  # Folder-like path in S3
+      region         = "us-east-2"
+      encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-2" # Change to your preferred AWS region }
 }
