69a322b2c589497feb995f56b6463c50623ed3a1
abdel-farah96 <abdulfarah1996@gmail.com>
2025-05-04 17:02:17 -0600

    uncomenting instance profile

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0030e9fc5c777545a" # Amazon Linux 2 AMI (Free Tier)
   instance_type = "t2.micro"              # Free-tier eligible instance
   key_name      = "sandbox-key-region-2"  # Replace with your SSH key pair name
-  # iam_instance_profile   = aws_iam_instance_profile.s3_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.s3_profile.name
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 
   tags = {
