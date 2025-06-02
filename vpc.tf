43a0435f466410f2d4d0276003019582c9b0d41d
abdel <abdulfarah1996@gmail.com>
2025-06-02 04:40:07 +0000

    adding public/private subnets and rts

--- vpc.tf
+++ vpc.tf
@@ -7,12 +7,85 @@ resource "aws_vpc" "custom_vpc" {
   }
 }
 
-resource "aws_subnet" "custom_subnet" {
+resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.custom_vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "custom_subnet"
+    Name = "public_subnet"
   }
 
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.custom_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private_subnet"
+  }
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+
+} 
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "NAT gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
